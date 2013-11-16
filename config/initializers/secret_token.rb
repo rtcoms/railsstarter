71b4ed7cd6770702c5f5c9926e37e5d9c9972238
Rohit Trivedi <86.rohit@gmail.com>
2013-11-16 20:53:51 +0530

    devise login worked

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Testlogin::Application.config.secret_key_base = '72da5a7b3edd1fb1d4f7cf20b5fe489e620d5659bc75731fdedafda74a8afa49e3cba0ef61b843c759020f4281c029b446fb93e60ee28c04f00ebff0f264a639'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Testlogin::Application.config.session_store :cookie_store, key: '_testlogin_session'
